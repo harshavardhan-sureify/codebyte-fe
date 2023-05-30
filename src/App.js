41e4ba52cd85254da184e289acbc7dd21f53d650
harshavardhan-sureify <harshavardhan.sivadhanam@sureify.com>
2023-05-30 10:20:18 +0530

    minor bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import PollCreate from "./components/pollcreation/PollCreation";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import AppRoutes from "./components/routes/AppRoutes";
