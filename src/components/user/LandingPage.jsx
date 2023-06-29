f85896e9fdaed70e2d110121679a4c2fac76f5f8
harshavardhan-sureify <harshavardhan.sivadhanam@sureify.com>
2023-06-29 23:11:35 +0530

    made the changes to the title and logout button

--- src/components/user/LandingPage.jsx
+++ src/components/user/LandingPage.jsx
@@ -1,13 +1,16 @@
 import { useTheme } from "@emotion/react";
-import { Box } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { StyledOutletContainer, StyledSideBar } from "../Styles";
-import { auth } from "../features/User.reducer";
-import { useSelector } from "react-redux";
+import { auth, logout } from "../features/User.reducer";
+import { useDispatch, useSelector } from "react-redux";
 import { ADMIN_ROUTES, USER_ROLE, USER_ROUTES } from "../../constants";
+import LogoutIcon from "@mui/icons-material/Logout";
 export const LandingPage = () => {
     const { role } = useSelector(auth);
+    const dispatch  = useDispatch();
+    const navigate = useNavigate();
     const [routes, setRoutes] = useState([]);
     useEffect(() => {
         if (role === USER_ROLE) {
@@ -29,14 +32,60 @@ export const LandingPage = () => {
             textAlign: "center",
         };
     };
+    const handleLogout = () =>{
+        dispatch(logout());
+        navigate("/login");
+    }
     return (
         <Box sx={{ display: "flex" }}>
-            <StyledSideBar sx={{display:{sm:"none",xs:"none",md:"block",lg:"block"}}}>
-                {routes.map((route) => (
-                    <NavLink to={route.route} style={activeStyles} key = {`navlink${route.name}`}>
-                        {route.name}
-                    </NavLink>
-                ))}
+            <StyledSideBar
+                sx={{
+                    display: {
+                        sm: "none",
+                        xs: "none",
+                        md: "block",
+                        lg: "block",
+                    },
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        height: "100%",
+                    }}
+                >
+                    <Box>
+                        {routes.map((route) => (
+                            <NavLink
+                                to={route.route}
+                                style={activeStyles}
+                                key={`navlink${route.name}`}
+                            >
+                                {route.name}
+                            </NavLink>
+                        ))}
+                    </Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            p: 2,
+                        }}
+                    >
+                        <IconButton
+                            variant="contained"
+                            color="error"
+                            sx={{ px: 3, py: 1 ,color:'red',fontWeight:"",fontSize:"16px"}}
+                            onClick={handleLogout}
+                            size="medium"
+                        >
+                            <LogoutIcon/>  LOGOUT
+                        </IconButton>
+                    </Box>
+                </Box>
             </StyledSideBar>
             <StyledOutletContainer>
                 <Outlet />
