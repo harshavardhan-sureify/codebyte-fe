4a8c6ca06a0efac26653246a5988eb8b3c779a1d
harshavardhan-sureify <harshavardhan.sivadhanam@sureify.com>
2023-07-03 10:36:24 +0530

    minor changes

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const GO_BASE_URL = "http://localhost:8080/api/v1/";
-const PHP_BASE_URL = "http://localhost/api/v1/";
+const GO_BASE_URL = "http://192.168.129.231:8080/api/v1/";
+const PHP_BASE_URL = "http://192.168.129.231:80/api/v1/";
 
 const createPollApi = PHP_BASE_URL + "createpoll";
 const signUp = GO_BASE_URL + "signup";
@@ -19,61 +19,61 @@ const forgotPassword = GO_BASE_URL + "user/forgotpassword";
 const validateOtp = GO_BASE_URL + "user/validateotp";
 const resetPassword = GO_BASE_URL + "user/changepassword";
 export const ADMIN_ROUTES = [
-  {
-    name: "dashboard",
-    route: "/admin/dashboard",
-  },
-  {
-    name: "Active polls",
-    route: "/admin/activepolls",
-  },
-  {
-    name: "All polls",
-    route: "/admin/allpolls",
-  },
-  {
-    name: "All users",
-    route: "/admin/allusers",
-  },
-  {
-    name: "profile",
-    route: "/admin/profile",
-  },
+    {
+        name: "dashboard",
+        route: "/admin/dashboard",
+    },
+    {
+        name: "Active polls",
+        route: "/admin/activepolls",
+    },
+    {
+        name: "All polls",
+        route: "/admin/allpolls",
+    },
+    {
+        name: "All users",
+        route: "/admin/allusers",
+    },
+    {
+        name: "profile",
+        route: "/admin/profile",
+    },
 ];
 export const USER_ROUTES = [
-  {
-    name: "dashboard",
-    route: "/user/dashboard",
-  },
-  {
-    name: "Answered polls",
-    route: "/user/answeredpolls",
-  },
-  {
-    name: "profile",
-    route: "/user/profile",
-  },
+    {
+        name: "dashboard",
+        route: "/user/dashboard",
+    },
+    {
+        name: "Answered polls",
+        route: "/user/answeredpolls",
+    },
+    {
+        name: "profile",
+        route: "/user/profile",
+    },
 ];
 export const USER_ROLE = "user";
 export const ADMIN_ROLE = "admin";
 
 export {
-  createPollApi,
-  GO_BASE_URL,
-  PHP_BASE_URL,
-  signUp,
-  loginApi,
-  adminDashboardApi,
-  allUsers,
-  addUser,
-  ACTIVE_POLLS_URL,
-  ANSWERED_POLLS_URL,
-  confirmuser,
-  userInfo,
-  updateUserProfile,
-  deleteUserApi,
-  forgotPassword,
-  validateOtp,
-  resetPassword,
-  allPollsUrl
+    createPollApi,
+    GO_BASE_URL,
+    PHP_BASE_URL,
+    signUp,
+    loginApi,
+    adminDashboardApi,
+    allUsers,
+    addUser,
+    ACTIVE_POLLS_URL,
+    ANSWERED_POLLS_URL,
+    confirmuser,
+    userInfo,
+    updateUserProfile,
+    deleteUserApi,
+    forgotPassword,
+    validateOtp,
+    resetPassword,
+    allPollsUrl,
 };
